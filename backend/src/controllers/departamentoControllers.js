a0cda040ca54370b42da8033132ce15e6aeb829d
Daniel <danielronaldmutumbajoy@gmail.com>
2024-12-15 13:30:15 -0500

    creation of routes and functions
    Creation of routes and controllers for requests

---
+++ backend/src/controllers/departamentoControllers.js
@@ -0,0 +1,28 @@
+import { db } from "../database.js"
+
+//Registrar un departamento
+export const registerDepartment = async(req,res) => {
+
+
+};
+
+//Ver los departamentos
+export const viewDepartment = async(req,res) => {
+
+
+};
+
+//Actualizar un departamento
+export const updateDepartment = async(req, res) => {
+
+
+};
+
+
+//Eliminar un departamento
+export const deleteDepartment = async(req, res) => {
+
+};
+
+
+
